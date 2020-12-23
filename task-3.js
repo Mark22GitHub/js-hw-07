64cd30038cfa198ee6823454c077bc7e5ce5b5dc
Mark22GitHub <atelezhenko93@gmail.com>
2020-12-23 23:45:37 +0200

    1,2,3,4,5,7

--- task-3.js
+++ task-3.js
@@ -2,14 +2,10 @@
 
 // Напиши скрипт для создания галлереи изображений по массиву данных.
 
-// В HTML есть список ul#gallery.
-// <ul id="gallery"></ul>
-
 // Используй массив объектов images для создания тегов img вложенных в li. Для создания разметки используй шаблонные строки и insertAdjacentHTML().
 
 // Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 // Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.
-
 
 const images = [
   {
@@ -28,3 +24,10 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+
+const ul = document.querySelector('#gallery');
+
+images.forEach(img => {
+  ul.insertAdjacentHTML('beforeend', `<li><img src="${img.url}" alt=${img.alt} width='300'></li>`);
+})
--- task-4.js
+++ task-4.js
@@ -5,12 +5,6 @@
 // Создай функции increment и decrement для увеличения и уменьшения значения счетчика
 // Добавь слушатели кликов на кнопки, вызовы функций и обновление интерфейса
     
-{/* <div id="counter">
-  <button type="button" data-action="decrement">-1</button>
-  <span id="value">0</span>
-  <button type="button" data-action="increment">+1</button>
-</div> */}
-
 let counterValue = 0;
 
 const increment = document.querySelector('[data-action="increment"]');
